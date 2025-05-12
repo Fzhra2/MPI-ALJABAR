b0152e45ab7ab1d343524b0b6297c70983484040
Fzhra2 <ftmhzahra50@gmail.com>
2025-05-12 14:27:47 +0700

    Media Aljabar

---
+++ story_content/user.js
@@ -0,0 +1,43 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6RHoMsI0Id5":
+        Script1();
+        break;
+      case "6QJgBz7oliu":
+        Script2();
+        break;
+      case "6McGOf6KJJ1":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSongku');
+audio.src="Happy.mp3";
+audio.load();
+audio.play();
+audio.volume=0.5;
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSongku');
+audio.src="Happy.mp3";
+audio.load();
+audio.play();
+audio.volume=0.5;
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSongku');
+audio.src="Happy.mp3";
+audio.load();
+audio.play();
+audio.volume=0.5;
+}
+
